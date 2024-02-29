f38e1de4b93829cae71c259e33215882b4dcc3ef
MIkita <115146055+mikitavydrankou@users.noreply.github.com>
2024-02-29 17:22:33 +0100

    commit

--- Wariant 102/cmake-build-debug/cmake_install.cmake
+++ Wariant 102/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/mikita/Documents/PS172763/lab_2
+# Install script for directory: /Users/mikita/Documents/PS172763/Wariant 102
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/mikita/Documents/PS172763/lab_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/mikita/Documents/PS172763/Wariant 102/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ Wariant 102/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    printf("%lf", (1/a)+(b/(2+c)));
+    return 0;
+}
