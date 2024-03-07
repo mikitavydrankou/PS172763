e05934d0a810060a33005dfb3905de74400950c3
MIkita <115146055+mikitavydrankou@users.noreply.github.com>
2024-03-07 18:56:10 +0100

    commit

---
+++ lab_3/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+    //zad1
+//int SumTwoNumbers(int a, int b){
+//    return a+b;
+//}
+
+    //zad3
+//int calculateFactorial(int a){
+//        int  factorial = 1;
+//        for (int i = 1; i <= a; i++) {
+//            factorial *= i;
+//        }
+//        return factorial;
+//}
+
+    //zad5
+//int sumSquares(int n){
+//    int wynik = 0;
+//    for(int i = 0; i <= n; i++){
+//        wynik+=(i*i);
+//    }
+//    return wynik;
+//}
+
+    //zad6
+//int calculateSquareRootFloor(int n){
+//    int i = 0;
+//    while(i*i<=n){
+//        i++;
+//    }
+//    return i-1;
+//}
+
+    //zad8
+//int calculateFactorialRecursively(int n) {
+//    if (n == 0) {
+//        return 1;
+//    } else {
+//        return n * calculateFactorialRecursively(n - 1);
+//    }
+//}
+
+int main() {
+
+    return 0;
+}
