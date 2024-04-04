c025e8a74a1fac85c700aadbb659d6551f7b7275
MIkita <115146055+mikitavydrankou@users.noreply.github.com>
2024-04-04 17:21:51 +0200

    commit

---
+++ Wariant704/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int count_negative_elements(int n, int arr[n]){
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i]<0){
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int tab[5] = {-1, 2, 3, -4, -5};
+    printf("%d\n", count_negative_elements(5, tab));
+    return 0;
+}
